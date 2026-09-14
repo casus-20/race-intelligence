9b37b6b5d9d0e283554b4c6f75985e9027f1c70c
casus-20 <levent.asar@hotmail.com>
2026-09-14 18:50:48 +0300

    Update tjk_fetch.py

--- tjk_fetch.py
+++ tjk_fetch.py
@@ -28,9 +28,9 @@ CITY_IDS = {
     "Bursa": 4,
     "İzmir": 2,
     "Adana": 1,
-    "Elazığ": 6,
+    "Elazığ": 7,
     "Diyarbakır": 8,
-    "Şanlıurfa": 7,
+    "Şanlıurfa": 6,
     "Antalya": 10,
 }
 
